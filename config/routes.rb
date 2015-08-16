22faa620781c5c2ebda8fd612788924daa57947f
marina starkova <marina.starkova@gmail.com>
2015-08-16 23:13:08 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     get 'history', to: 'high_voltage/pages#show', id: 'history', as: 'history'
     get 'philosophy', to: 'high_voltage/pages#show', id: 'philosophy', as: 'philosophy'
     get 'types', to: 'high_voltage/pages#show', id: 'types', as: 'types'
+    get 'types/blue', to: 'high_voltage/pages#show', id: 'blue', as: 'blue'
+    get 'types/black', to: 'high_voltage/pages#show', id: 'black', as: 'black'
     get 'vineyards', to: 'high_voltage/pages#show', id: 'vineyards', as: 'vineyards'
     get 'contact', to: 'high_voltage/pages#show', id: 'contact', as: 'contact'
     get 'film', to: 'high_voltage/pages#show', id: 'film', as: 'film'
