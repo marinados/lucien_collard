1ffd35dd2add9a7cea0d49eabad7dd1886aea10e
marina starkova <marina.starkova@gmail.com>
2015-08-16 22:08:28 +0200

    js

--- app/assets/javascripts/types.js
+++ app/assets/javascripts/types.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
   if ($('.arrows').length == 1) {
-    console.log('right');
     $('#right-arrow').click(function(){
-      $(document.body).css({
-        'background': 'url(assets/backgrounds/8A.jpg)',
-        'background-size': 'cover'
-      });
+      var bg = $('.fullscreen');
+      bg.fadeOut(800).queue(function(){
+        if(bg.css('background').search('8cap.jpg') >= 0) {
+          $(this).css('background', 'url(assets/backgrounds/8blue.jpg)');
+         } else if (bg.css('background').search('8blue.jpg') >= 0) {
+          $(this).css('background', 'url(assets/backgrounds/8black.jpg)');
+         } else if (bg.css('background').search('8black.jpg') >= 0) {
+          $(this).css('background', 'url(assets/backgrounds/8cap.jpg)');
+         }
+           $(this).css('background-size', 'cover');
+           $(this).dequeue();
+       });
+       bg.fadeIn(1200)
     });
   }
 })
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def random_bg
     if params[:id] == "types"
-      "assets/backgrounds/8.jpg"
+      "assets/backgrounds/8cap.jpg"
     else
       "assets/backgrounds/#{rand(6)}.jpg"
     end
