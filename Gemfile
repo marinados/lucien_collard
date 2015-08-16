f9652e3be3a8a19f2185b737d593d7deb1659c21
marina starkova <marina.starkova@gmail.com>
2015-08-16 19:19:43 +0200

    types

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'font-awesome-sass', '~> 4.3.0'
 
+gem "autoprefixer-rails"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def random_bg
     if params[:id] == "types"
-      "assets/backgrounds/7.jpg"
+      "assets/backgrounds/8.jpg"
     else
       "assets/backgrounds/#{rand(6)}.jpg"
     end
