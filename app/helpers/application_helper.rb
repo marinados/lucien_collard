4d3f099465f3fe7ab3e33d1db1230e3a3a042e35
marina starkova <marina.starkova@gmail.com>
2015-09-05 01:02:37 +0200

    bg solved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,17 @@ module ApplicationHelper
       'bg7'
     end
   end
+
+  def changing_bg
+    case params[:id]
+    when 'history'
+      'bg-history1'
+    when 'philosophy'
+      'bg-philosophy1'
+    when 'vineyards'
+      'bg-vineyards1'
+    else
+      nil
+    end
+  end
 end
