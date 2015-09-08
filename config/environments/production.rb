182d3e580d508f116b225534a7e6ff2c02e26806
marina starkova <marina.starkova@gmail.com>
2015-09-08 22:38:19 +0200

    figaro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_address'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :user_name            => ENV["gmail_address"],
+     :password             => ENV["gmail_password"],
+     :authentication       => "plain",
+    :enable_starttls_auto => true
   }
 end
