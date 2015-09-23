d777a3a212bc12cb98e4546853416e9b4b89b3d9
marina starkova <marina.starkova@gmail.com>
2015-09-24 00:08:38 +0200

    mail

--- config/application.rb
+++ config/application.rb
@@ -31,15 +31,15 @@ module Champagne
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    # config.action_mailer.delivery_method = :smtp
-    # # SMTP settings for gmail
-    # config.action_mailer.smtp_settings = {
-    #   :address              => "smtp.gmail.com",
-    #   :port                 => 587,
-    #   :user_name            => ENV["GMAIL_ADDRESS"],
-    #   :password             => ENV["GMAIL_PASSWORD"],
-    #   :authentication       => "plain",
-    #   :enable_starttls_auto => true
-    # }
+    config.action_mailer.delivery_method = :smtp
+    # SMTP settings for gmail
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => ENV["GMAIL_ADDRESS"],
+      :password             => ENV["GMAIL_PASSWORD"],
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+    }
   end
 end
