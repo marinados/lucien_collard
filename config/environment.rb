9776921d2d66a60266a2b4294a31a5647f82af1e
marina starkova <marina.starkova@gmail.com>
2015-10-27 23:15:53 +0100

    constant

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
 
 # Initialize the Rails application.
 Rails.application.initialize!
